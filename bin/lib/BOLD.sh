bf92e6b66c4d3e2da764d0303f7658afe1b816b2
da99 <i-hate-spam-1234567@mailinator.com>
2016-03-31 12:42:43 -0500

    Added: specs for BOLD

--- bin/lib/BOLD.sh
+++ bin/lib/BOLD.sh
@@ -1,15 +1,21 @@
 
 # === {{CMD}}  "my string that is {{ bold }}"
 
-source $THIS_DIR/bin/lib/COLORIZE.sh
+source $THIS_DIR/bin/lib/MULTI.sh
 
 specs () {
-  bash_setup BOLD  "my string that is {{ bold }}"
-  bash_setup BOLD  "this is also orange:  {{my {bold}}}"
+  local +x RESET='\e[0m'
+  local +x BOLD='\e[1m'
+
+  should-match-output "$(echo -e my string that is ${BOLD} bold ${RESET})" \
+    'mksh_setup BOLD  "my string that is {{ bold }}"'
+
+  should-match-output "$(echo -e this is also bold: ${BOLD}my {bold}${RESET})" \
+    'mksh_setup BOLD  "this is also bold: {{my {bold}}}"'
 }
 
 BOLD () {
-  COLORIZE "$(tput bold)" "$@"
+  COLOR="BOLD" MULTI "$@"
 }
 
 
