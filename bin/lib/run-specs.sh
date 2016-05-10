693bdffbbc8a05e003fcefd7245bbee989d5ad09
da99 <i-hate-spam-1234567@mailinator.com>
2016-05-10 00:38:23 -0500

    Fixed: testing standard error output

--- bin/lib/run-specs.sh
+++ bin/lib/run-specs.sh
@@ -111,6 +111,34 @@ should-exit () {
   fi
 }
 
+#  should-match-stderr  "EXPECT"   "my-cmd -with -args"
+#  should-match-stderr  "EXPECT"   "ACTUAL"
+should-match-stderr () {
+  local +x EXPECT="$1"; shift
+  local +x CMD="$1"; shift
+  local +x STAT
+  local +x ACTUAL
+
+  set +e
+  ACTUAL="$(eval "$CMD" 2>&1)"
+  STAT="$?"
+  set -e
+
+  # if [[ "$(echo -E $ACTUAL)" != "$(echo -E $EXPECT)" ]]; then
+  if [[ "$ACTUAL" != "$EXPECT" ]]; then
+    if [[ -z "$ACTUAL" ]]; then
+      ACTUAL="[NULL STRING]"
+    fi
+    exec >&2
+    echo -e -n "!!! MISMATCH: (actual) \"${Red}"; echo -E -n "$ACTUAL";
+    echo -e -n "${Color_Off}\"  !=  (expected) \""; echo -E    "$EXPECT\""
+    echo       "    CMD: $CMD"
+    exit 1
+  else
+    mksh_setup GREEN "=== {{Passed}}: $(echo -E $CMD)"
+  fi
+} # === should-match-output
+
 #  should-match-output  "EXPECT"   "my-cmd -with -args"
 #  should-match-output  "EXPECT"   "ACTUAL"
 should-match-output () {
@@ -130,8 +158,9 @@ should-match-output () {
       ACTUAL="[NULL STRING]"
     fi
     exec >&2
-    echo -e -n "=== MISMATCH: \"${Red}"; echo -E -n "$ACTUAL";
-    echo -e -n "${Color_Off}\"  !=  \""; echo -E    "$EXPECT\""
+    echo -e -n "!!! MISMATCH: (actual) \"${Red}"; echo -E -n "$ACTUAL";
+    echo -e -n "${Color_Off}\"  !=  (expected) \""; echo -E    "$EXPECT\""
+    echo       "    CMD: $CMD"
     exit 1
   else
     mksh_setup GREEN "=== {{Passed}}: $(echo -E $CMD)"
