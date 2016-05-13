8c3265f6b323d8583667d4a083b023340a0105ef
da99 <i-hate-spam-1234567@mailinator.com>
2016-05-13 00:59:07 -0500

    Fixed: added boundaries to prevent partial matches.

--- bin/lib/between-lines.sh
+++ bin/lib/between-lines.sh
@@ -5,11 +5,17 @@ between-lines () {
   local +x FIRST="$1"; shift
   local +x SECOND="$1"; shift
 
-  grep -Pzo "(?s)$FIRST\n?\K(.+?)(?=$SECOND)"  "$@"
+  grep -Pzo "(?s)(\A|\n)$FIRST\n?\K(.+?)(?=\n$SECOND)"  "$@"
 } # === end function
 
 
 specs () {
   local +x ACTUAL="$(echo "a\nFIRST\nabc\ndef\nSECOND\n" | mksh_setup between-lines "FIRST"  "SECOND")"
+  should-match "$(echo -n "abc\ndef\n")"  "$ACTUAL"
+
+  local +x ACTUAL="$(echo "FIRST\na\n-- FIRST\nabc\ndef\n-- SECOND\n" | mksh_setup between-lines "-- FIRST"  "-- SECOND")"
+  should-match "$(echo -n "abc\ndef\n")"  "$ACTUAL"
+
+  local +x ACTUAL="$(echo "-- FIRST\na\nFIRST\nabc\ndef\nSECOND\n" | mksh_setup between-lines "FIRST"  "SECOND")"
   should-match "$(echo -n "abc\ndef\n")"  "$ACTUAL"
 } # === specs ()
