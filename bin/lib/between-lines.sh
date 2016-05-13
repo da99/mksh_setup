f1903be16429a8e45bb8a2e8d7d671e603f27e41
da99 <i-hate-spam-1234567@mailinator.com>
2016-05-13 00:46:24 -0500

    Added: between-lines

---
+++ bin/lib/between-lines.sh
@@ -0,0 +1,14 @@
+
+# === {{CMD}}  "PATTERN 1"   "PATTERN 2"
+between-lines () {
+  local +x FIRST="$1"; shift
+  local +x SECOND="$1"; shift
+
+  grep -Pzo "(?s)$FIRST\n?\K(.+?)(?=$SECOND)"
+} # === end function
+
+
+specs () {
+  local +x ACTUAL="$(echo "a\nFIRST\nabc\ndef\nSECOND\n" | mksh_setup between-lines "FIRST"  "SECOND")"
+  should-match "$(echo -n "abc\ndef\n")"  "$ACTUAL"
+} # === specs ()
