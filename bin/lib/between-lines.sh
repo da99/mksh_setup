58cb1aed4635b3224edbcf1ebfb6a42f298c7be8
da99 <i-hate-spam-1234567@mailinator.com>
2016-05-13 00:49:11 -0500

    Added: STDin or FILE name

--- bin/lib/between-lines.sh
+++ bin/lib/between-lines.sh
@@ -1,10 +1,11 @@
 
-# === {{CMD}}  "PATTERN 1"   "PATTERN 2"
+# === cat FILE | {{CMD}}  "PATTERN 1"   "PATTERN 2"
+# ===            {{CMD}}  "PATTERN 1"   "PATTERN 2"  "FILE"
 between-lines () {
   local +x FIRST="$1"; shift
   local +x SECOND="$1"; shift
 
-  grep -Pzo "(?s)$FIRST\n?\K(.+?)(?=$SECOND)"
+  grep -Pzo "(?s)$FIRST\n?\K(.+?)(?=$SECOND)"  "$@"
 } # === end function
 
 
