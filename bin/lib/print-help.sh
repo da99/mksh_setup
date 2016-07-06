2d736fc0b925ab1685d421763ea77ab8580af5a2
da99 <i-hate-spam-1234567@mailinator.com>
2016-07-06 01:53:44 -0500

    Refactoring.

--- bin/lib/print-help.sh
+++ bin/lib/print-help.sh
@@ -1,8 +1,52 @@
 
 source "$THIS_DIR/bin/lib/COLORIZE.sh"
 
-# === {{CMD}}
+# === {{CMD}} dir/path
+# ===   Prints all *.sh files in dir
+# === {{CMD}} path/to/file
+# ===   If file is in a dir called "bin/", and file is alongside
+# ===   another directory "lib",
+# ===   then all the files in lib/ are also printed.
 print-help () {
+
+  local +x TARGET="$1"; shift
+  local +x DIR="$(dirname "$(realpath "$TARGET")")"
+  local +x DIRNAME="$(basename "$DIR")"
+  local +x LIB="$DIR/lib"
+
+  if [[ -d "$TARGET" ]]; then
+    print-dir "$TARGET"
+    return 0
+  fi
+
+  if [[ "$DIRNAME" == "bin" && -d "$LIB" ]]; then
+    print-file "$TARGET"
+    print-dir "$LIB"
+    return 0
+  fi
+
+  if [[ -s "$TARGET" ]]; then
+    print-file "$TARGET"
+    return 0
+  fi
+
+  mksh_setup RED "=== Help not available for: "$TARGET""
+  exit 1
+
+} # === end function
+
+
+print-dir () {
+  local +x DIR="$1"; shift
+  local +x IFS=$'\n'
+  for FILE in $(find "$DIR/" -mindepth 1 -maxdepth 1 -type f -name "*.sh" -print); do
+    print-file "$FILE"
+  done
+} # === print-dir
+
+print-file () {
+  # === Accepts one arguments: one file.
+
   local +x Color_Off='\e[0m'
   local +x Bold="$(tput bold)"
   local +x Reset='\e[0m'
@@ -17,15 +61,14 @@ print-help () {
   # ===      that are followed by: # ===
   # === $ mksh_setup print-help path/to/file
 
-  local +x file="$1"; shift
+  local +x FILE="$1"; shift
   local +x cmd_print=""
   local +x cmd=""
   local +x nl_printed=""
-  local +x BIN_NAME="$(basename "$file")"
+  local +x BIN_NAME="$(basename "$FILE")"
   local +x IFS=$'\n'
 
-
-  for line in $(cat $file); do
+  for line in $(cat $FILE); do
     if echo "$line" | grep -P '^[\*\ +0-9A-Za-z\_\-\"'"\']+\)"'$' >/dev/null; then
       cmd=$(echo $line | tr -d '"'')')
       cmd_print=""
@@ -48,20 +91,16 @@ print-help () {
 
   done
 
-  dir_functions="$(dirname "$file")/lib"
-  [[ ! -d "$dir_functions" ]] && exit 0 || :
-
-  local +x IFS=$'\n'
-  for FILE in $(find "$dir_functions" -maxdepth 1 -mindepth 1 -type f -iname "*.sh" -print | sort); do
-    FUNCTION_NAME="$(basename "$FILE" .sh)"
-    FINAL="=== BOLD{{$FUNCTION_NAME}}"
-    for LINE in $(cat "$FILE" | grep -P  '^# ==='); do
-      LINE=${LINE/'# ==='/}
-      LINE=${LINE//'{{CMD}}'/"GREEN{{$BIN_NAME}}  ${FUNCTION_NAME} "}
-      LINE=${LINE//'{{BIN}}'/"BOLD{{$BIN_NAME}}"}
-      LINE=${LINE//'{{NAME}}'/"GREEN{{$FUNCTION_NAME}}"}
-      FINAL="$FINAL\n  $LINE"
-    done
-    COLORIZE "$FINAL"
+  FUNCTION_NAME="$(basename "$FILE" .sh)"
+  FINAL="=== BOLD{{$FUNCTION_NAME}}"
+  for LINE in $(cat "$FILE" | grep -P  '^# ==='); do
+    LINE=${LINE/'# ==='/}
+    LINE=${LINE//'{{CMD}}'/"GREEN{{$BIN_NAME}}  ${FUNCTION_NAME} "}
+    LINE=${LINE//'{{BIN}}'/"BOLD{{$BIN_NAME}}"}
+    LINE=${LINE//'{{NAME}}'/"GREEN{{$FUNCTION_NAME}}"}
+    FINAL="$FINAL\n  $LINE"
   done
-} # === end function
+  COLORIZE "$FINAL"
+} # === print-file
+
+
