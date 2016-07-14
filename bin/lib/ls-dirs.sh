538b72494f0c0507baaf1e50e08e49e2c951faa9
da99 <i-hate-spam-1234567@mailinator.com>
2016-07-14 17:58:54 -0500

    Added: filter out trash and lost+found

--- bin/lib/ls-dirs.sh
+++ bin/lib/ls-dirs.sh
@@ -1,7 +1,21 @@
 
 # === {{CMD}}
-# === List directories, ignoring ./ , .git , and maxdepth of 1, mindepth of 1
+# === {{CMD}}  DIR  [DIR]  [DIR]
+# === List directories, ignoring ./ , .git, lost+found, .Trash, .Trash-1000 , and maxdepth of 1, mindepth of 1
 # === Source: find . -maxdepth 1 -mindepth 1 -type d -not -name ".git" -print
 ls-dirs () {
-  find . -maxdepth 1 -mindepth 1 -type d -not -name ".git" -print
+  if [[ -z "$@" ]]; then
+    ls-dirs "$PWD"
+    return 0
+  fi
+
+  while [[ ! -z "$@" ]]; do
+    find -L $1 -maxdepth 1 -mindepth 1 -type d \
+      -not -name ".git"                    \
+      -and -not -name "lost+found"         \
+      -and -not -name ".Trash"             \
+      -and -not -name ".Trash-1000"        \
+      -print
+    shift
+  done
 } # === end function
