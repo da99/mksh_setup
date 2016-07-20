ddf98d5fbc627adce07c4f4a6aba1fd2524a9b2f
da99 <i-hate-spam-1234567@mailinator.com>
2016-07-19 19:28:38 -0500

    Added: help, bin file, etc. to name-to-func.

--- bin/public/name-to-func/_.sh
+++ bin/public/name-to-func/_.sh
@@ -7,15 +7,19 @@
 # === Exits 1 if file not found.
 name-to-func () {
   local +x APP_DIR="$1"; shift
-  local +x NAME="$1"; shift
-  local +x LIB_FILE=""
+  if [[ -f "$APP_DIR" ]]; then
+    APP_DIR="$( dirname "$(dirname "$(readlink -m "$APP_DIR")")" )"
+  fi
 
+  local +x NAME="$1"; shift
   if [[ "$NAME" == '--help' || "$NAME" == '-h' ]]; then
     NAME="help"
   fi
 
   run-file () {
     local +x LIB_FILE="$1"; shift
+
+    export "THIS_DIR"="$APP_DIR"
 
     if [[ -f "$LIB_FILE" ]]; then
       source "$LIB_FILE"
@@ -30,7 +34,16 @@ name-to-func () {
   run-file "$APP_DIR/bin/public/${NAME}.sh"    "$@"
   run-file "$APP_DIR/bin/lib/${NAME}.sh"       "$@"
 
+  if [[ "$NAME" == 'help' ]]; then
+    mksh_setup print-help $0
+    exit 0
+  fi
+
   # === It's an error:
+  if [[ -z "$NAME" ]]; then
+    NAME='[NULL]'
+  fi
+
   mksh_setup RED "!!! Unknown action: {{$NAME}}"
   exit 1
 } # === name-to-func
