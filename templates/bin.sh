0f0d9d783915e7c92a7848257eabda7591d14677
da99 <i-hate-spam-1234567@mailinator.com>
2017-03-28 19:29:08 -0500

    Dev. checkpoint: Touchup.

--- templates/bin.sh
+++ templates/bin.sh
@@ -4,16 +4,12 @@
 THE_ARGS="$@"
 THIS_DIR="$(dirname "$(dirname "$0")")"
 
-if [[ -z "$@" ]]; then
-  action="watch"
-else
-  action=$1
-  shift
-fi
+ACTION="$1"; shift
 
 set -u -e -o pipefail
 
-case $action in
+case $ACTION in
+
   help|--help|-h)
     PATH="$PATH:$THIS_DIR/../mksh_setup/bin"
     mksh_setup print-help $0 "$@"
@@ -21,35 +17,35 @@ case $action in
 
   *)
 
-    FUNC_FILE="$THIS_DIR/bin/public/${action}/_.sh"
+    FUNC_FILE="$THIS_DIR/bin/public/${ACTION}/_.sh"
 
     if [[ -s  "$FUNC_FILE"  ]]; then
 
       export THIS_FILE="$FUNC_FILE"
-      export THIS_FUNC="$action"
-      export THIS_FUNC_DIR="$THIS_DIR/bin/public/${action}"
+      export THIS_FUNC="$ACTION"
+      export THIS_FUNC_DIR="$THIS_DIR/bin/public/${ACTION}"
 
       source "$THIS_FILE"
       "$THIS_FUNC" "$@"
       return 0
     fi
 
-    BIN_FILE="$THIS_DIR/bin/lib/${action}.sh"
+    BIN_FILE="$THIS_DIR/bin/lib/${ACTION}.sh"
     if [[ -s "$BIN_FILE" ]]; then
       source "$BIN_FILE"
-      "$action" "$@"
+      "$ACTION" "$@"
       exit 0
     fi
 
     # === Check progs/bin:
-    if [[ -f "$THIS_DIR/progs/bin/$action" ]]; then
+    if [[ -f "$THIS_DIR/progs/bin/$ACTION" ]]; then
       export PATH="$THIS_DIR/progs/bin:$PATH"
-      "$THIS_DIR"/progs/bin/$action "$@"
+      "$THIS_DIR"/progs/bin/$ACTION "$@"
       exit 0
     fi
 
     # === It's an error:
-    echo "!!! Unknown action: $action" 1>&2
+    echo "!!! Unknown action: $ACTION" 1>&2
     exit 1
     ;;
 
